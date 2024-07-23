1dbe8f560dcba47f6c0f57fb47d2c70e394513ba
loopCoderMaster <132823027+loopCoderMaster@users.noreply.github.com>
2024-07-23 11:30:11 +0200

    Update postCreateCommand.sh

--- scripts/postCreateCommand.sh
+++ scripts/postCreateCommand.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
-set -e
-
 echo "Start postCreateCommand.sh script."
 
-# Imposta il working directory alla root del progetto
-#cd /workspaces/${localWorkspaceFolderBasename}
-#cd /workspaces/dev
-
 # Avvia i servizi Docker Compose in background
-docker-compose up -d
+if ! docker-compose up -d; then
+    echo "Errore durante l'avvio di Docker Compose. Controllare i log per maggiori dettagli."
+    docker-compose logs  # Mostra i log in caso di errore
+    exit 1  # Esci con un codice di errore
+fi
 
 # Verifica lo stato dei container
 echo "Verifica dello stato dei container..."
 docker-compose ps
 
-# Controlla i log dei container per eventuali errori
-echo "Controllo dei log dei container..."
-docker-compose logs
-
 # Esegui altri comandi post-creazione (opzionale)
-# Ad esempio, puoi installare dipendenze aggiuntive o eseguire script di inizializzazione
+# ...
